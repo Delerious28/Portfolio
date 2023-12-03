6f9e15e43ebb1c72250d75af3124288240ede28d
Delerious28 <118252809+Delerious28@users.noreply.github.com>
2023-12-03 23:38:26 +0100

    Delete script directory

--- script/scroll.js
+++
@@ -1,8 +0,0 @@
-function smoothScroll(target) {
-    const element = document.querySelector(target);
-    window.scroll({
-        top: element.getBoundingClientRect().top + window.scrollY,
-        left: 0,
-        behavior: 'smooth'
-    });
-}
